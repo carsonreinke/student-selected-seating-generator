e9a358de9e834ae6aaac2df5e643d67955c3e9b3
Carson Reinke <carson@reinke.co>
2020-04-04 10:46:58 -0400

    Add missing specs Updating configuration for deployment

---
+++ precache-manifest.298490ac1cea4d543a630501a7eb7084.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08fcaffe3d27651936d",
+    "url": "/css/app.5cb264ea.css"
+  },
+  {
+    "revision": "9d5cf0655ff73b96c6f8",
+    "url": "/css/chunk-vendors.2a88d96b.css"
+  },
+  {
+    "revision": "dfcc83ae7bf1061884fe07c2d30efde6",
+    "url": "/img/add.dfcc83ae.svg"
+  },
+  {
+    "revision": "88ef865549f3c7416d9fc4c2a5f620b8",
+    "url": "/img/arrange.88ef8655.svg"
+  },
+  {
+    "revision": "009be469e007eb49cd95248e8b5ce9d2",
+    "url": "/img/back.009be469.svg"
+  },
+  {
+    "revision": "e7403043f0aed4f12ca52af62bd2e745",
+    "url": "/img/delete.e7403043.svg"
+  },
+  {
+    "revision": "166fef72fb6e1e05f4637d5decccdc02",
+    "url": "/img/desk.166fef72.svg"
+  },
+  {
+    "revision": "23dd63ac1eb285753adcd7b4206d6559",
+    "url": "/img/drag.23dd63ac.svg"
+  },
+  {
+    "revision": "cb46d2ce407f0b881ca7c1d115b79a10",
+    "url": "/img/edit.cb46d2ce.svg"
+  },
+  {
+    "revision": "72ada97a873338ea115d9c69cd51ea5e",
+    "url": "/img/forward.72ada97a.svg"
+  },
+  {
+    "revision": "1e46e84ab96ea30bd85b081d5897322c",
+    "url": "/img/print.1e46e84a.svg"
+  },
+  {
+    "revision": "8ed0114eeeb181280864b5ec54503038",
+    "url": "/img/rotate.8ed0114e.svg"
+  },
+  {
+    "revision": "72c46d2ac25d729514de4e3c8a43cb12",
+    "url": "/img/save.72c46d2a.svg"
+  },
+  {
+    "revision": "0e2320860a90be534d9e113838c91195",
+    "url": "/img/start-over.0e232086.svg"
+  },
+  {
+    "revision": "d307862168ceb88485b7fb4c780d3cb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08fcaffe3d27651936d",
+    "url": "/js/app.8a5665e0.js"
+  },
+  {
+    "revision": "9d5cf0655ff73b96c6f8",
+    "url": "/js/chunk-vendors.42e61974.js"
+  },
+  {
+    "revision": "dadb8afb31cd7ef331a01815638ead0f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/robots.txt"
+  }
+]);
